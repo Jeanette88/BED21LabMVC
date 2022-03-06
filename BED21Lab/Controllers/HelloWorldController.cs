faa69ab3c88b6dd5e4943cb4d2c2c1872f9a1fa6
beijbe <jv_88_@hotmail.com>
2022-03-06 17:03:18 +0100

    Add project files.

---
+++ BED21Lab/Controllers/HelloWorldController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BED21Lab.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BED21Lab/Controllers/PhotosController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BED21Lab.Controllers
+{
+    public class PhotosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cosmos()
+        {
+            return View();
+        }
+        public IActionResult Dahlia
+            ()
+        {
+            return View();
+        }
+        public IActionResult Lotus()
+        {
+            return View();
+        }
+
+        public ActionResult photoView(string id)
+        {
+            ViewBag.photo = id;
+            return View();
+        }
+    }
+}
